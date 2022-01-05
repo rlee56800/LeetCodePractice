351be852f29f9dd482b64703149bbe7984561df2
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-04 17:29:09 -0800

    Create Zigzag Conversion.py

---
+++ Jan4_2022/Zigzag Conversion.py
@@ -0,0 +1,41 @@
+'''The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+'''
+
+#ALMOST WORKS
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        largest = (numRows - 1) * 2
+        alt = current = largest
+        ret_str = ""
+        
+        for i in range(numRows):
+            counter = i
+            while counter < len(s):
+                ret_str += s[counter]
+                if counter%2 == 0:
+                    counter += current
+                else:
+                    counter += alt
+            
+            current = current-2 if current != 2 else largest
+            alt = alt+2 if alt != largest else 2
+        
+        return ret_str
+            
+        
