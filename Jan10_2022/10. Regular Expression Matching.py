7d87bbed451d2ce1405d58b64dd3fccb39652a20
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-10 01:40:06 -0800

    Update 10. Regular Expression Matching.py

--- Jan10_2022/10. Regular Expression Matching.py
+++ Jan10_2022/10. Regular Expression Matching.py
@@ -6,17 +6,18 @@ class Solution(object):
     :rtype: bool
     """
     
-      p_index = 0
-      for i in range(len(s)):
-          if p_index <= len(p) and i < len(s):
-              return False
-          if p[p_index] == '*':
-              p_index += -1
-          if p[p_index] != '.' and s[i] != p[p_index]:
-              return False
-          # else s[i] == p[p_index]; keep going
-          p_index += 1
-
-      return True
+    p_index = 0
+    for i in range(len(s)):
+        if p_index >= len(p):
+            return False
+        if p[p_index] == '*':
+            p_index += -1
+        if p[p_index] != '.' and s[i] != p[p_index]:
+            return False
+        # else s[i] == p[p_index]; keep going
+        p_index += 1
+    
+    return True
             
             
+# doesn't work for print(isMatch("aab", "c*a*b"))
