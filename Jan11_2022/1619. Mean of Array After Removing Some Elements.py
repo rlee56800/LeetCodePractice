bb58311d7dbd3d0c8c642b0983127476ed00da67
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-11 02:03:46 -0800

    Completed

--- Jan11_2022/1619. Mean of Array After Removing Some Elements.py
+++ Jan11_2022/1619. Mean of Array After Removing Some Elements.py
@@ -2,7 +2,7 @@
 Given an integer array arr, return the mean of the remaining integers after removing the smallest 5% and the largest 5% of the elements.
 
 Answers within 10-5 of the actual answer will be considered accepted.
-'''
+''' 
 
 import math
 class Solution(object):
