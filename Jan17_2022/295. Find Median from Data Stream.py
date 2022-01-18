ac62b12a639638940fd22737f3d30be50557e4a6
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-18 00:28:38 -0800

    Wrong Answer
    Doesn't work if data is entered out of order :/

--- Jan17_2022/295. Find Median from Data Stream.py
+++ Jan17_2022/295. Find Median from Data Stream.py
@@ -1,36 +1,24 @@
-'''
-The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
-
-    For example, for arr = [2,3,4], the median is 3.
-    For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
-
-Implement the MedianFinder class:
-
-    MedianFinder() initializes the MedianFinder object.
-    void addNum(int num) adds the integer num from the data stream to the data structure.
-    double findMedian() returns the median of all elements so far. Answers within 10^-5 of the actual answer will be accepted.
-
-'''
-
 class MedianFinder:
     
     def __init__(self):
         self.nums_list = []
 
     def addNum(self, num: int) -> None:
-        #self.nums_list.append(num)
-        for i in range(len(self.nums_list)):
-            if num < self.nums_list[i]:
-                self.nums_list.insert(i, num)
-                return
-        self.nums_list.append(num)
+        if not self.nums_list: # no numbers in list
+            self.nums_list.append(num)
+        elif len(self.nums_list) == 1: # if only one number in list
+            self.nums_list.append(num)
+            self.nums_list.sort()
+        else: # if 2 numbers in list
+            self.nums_list.append(num)
+            self.nums_list.sort()
+            self.nums_list = [self.nums_list[1]]
 
     def findMedian(self) -> float:
-        half_val = len(self.nums_list)//2
-        if len(self.nums_list)%2 == 0:
-            return (self.nums_list[half_val - 1] + self.nums_list[half_val])/2.0
+        if len(self.nums_list) == 1:
+            return self.nums_list[0]
         else:
-            return self.nums_list[half_val]
+            return sum(self.nums_list)/2.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
