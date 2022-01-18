56c53256089689f9db93b006d3afcfdda57d2d64
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-18 01:09:04 -0800

    Completed

--- Jan17_2022/605. Can Place Flowers.py
+++ Jan17_2022/605. Can Place Flowers.py
@@ -4,10 +4,13 @@ You have a long flowerbed in which some of the plots are planted, and some are n
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
 '''
 
-import math
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if sum(flowerbed) + n > math.ceil(len(flowerbed)/2):
-            return False
-        else:
-            return True
+        potential = 0
+        
+        for i in range(0, len(flowerbed)):
+            if (not flowerbed[i]) and (i == 0 or not flowerbed[i-1]) and (i == len(flowerbed)-1 or not flowerbed[i+1]):
+                flowerbed[i] = 1
+                potential += 1 
+        
+        return potential >= n
