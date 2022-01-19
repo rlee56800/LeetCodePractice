ea2c4788cb59a5b1dfdf1a4a429fb9a3bde5f8a4
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-01-18 23:05:10 -0800

    Completed

---
+++ Jan17_2022/142. Linked List Cycle II.py
@@ -0,0 +1,31 @@
+'''
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head:
+            return
+        
+        ll_list = []
+        current = head
+        
+        while current:
+            if current in ll_list:
+                return current #ll_dict[current]
+            else:
+                ll_list.append(current)
+                current = current.next
+        
+        return #ListNode(-1)
